465710c13322a0f532454965df1cfea2282c2fe8
Diamantino <Diamantino.op@gmail.com>
2025-03-11 12:42:19 +0100

    Update

--- src/kernel/kernel-x86_64/src/hal/GDT.hpp
+++ src/kernel/kernel-x86_64/src/hal/GDT.hpp
@@ -4,7 +4,7 @@
 #include "Types.hpp"
 
 namespace hal::x86_64 {
-	struct [[gnu::packed]] Tss {
+	struct __attribute__((packed)) Tss {
 		u32 _reserved;
 		Array<u64, 3> rsp;
 		u64 _reserved1;
@@ -15,29 +15,47 @@ namespace hal::x86_64 {
 		u16 iopbOffset;
 	};
 
+	enum Selector {
+		ZERO = 0,
+		KERNEL_CODE = 1,
+		KERNEL_DATA = 2,
+		USER_DATA = 3,
+		USER_CODE = 4,
+		TSS = 5,
+	};
+
+	// User: Ring 3, Driver: Ring 2, System: Ring 1
+	enum AccessBytes : u32 {
+		PRESENT = 0b10000000,
+		USER = 0b01100000,
+		DRIVER = 0b01000000,
+		SYSTEM = 0b00100000,
+		CD_SEGMENT = 0b00010000,
+		EXECUTABLE = 0b00001000,
+		CONF_DIR = 0b00000100,
+		READ_WRITE = 0b00000010,
+		ACCESSED = 0b00000001,
+	};
+
+	enum Flags {
+		PAGE_GRANULARITY = 0b1000,
+		PROTECTED_SEGMENT = 0b0100,
+		LONG_MODE = 0b0010,
+	};
+
+	// 64-Bit ignores limit and base values.
 	struct __attribute__((packed)) GdtEntry {
 		u16 limitLow{};
 		u16 baseLow{};
 		u8 baseMid{};
-		u8 flags{};
+		u8 accessByte{};
 		u8 limitHigh : 4 {};
-		u8 granularity : 4 {};
+		u8 flags : 4 {};
 		u8 baseHigh{};
 
 		constexpr GdtEntry() = default;
 
-		constexpr GdtEntry(u8 flags, u8 granularity)
-			: flags(flags),
-			  granularity(granularity) {};
-
-		constexpr GdtEntry(u32 base, u32 limit, u8 flags, u8 granularity)
-			: limitLow(limit & 0xffff),
-			  baseLow(base & 0xffff),
-			  baseMid((base >> 16) & 0xff),
-			  flags(flags),
-			  limitHigh((limit >> 16) & 0x0f),
-			  granularity(granularity),
-			  baseHigh((base >> 24) & 0xff) {}
+		explicit GdtEntry(u8 accessByte, u8 flags): accessByte(accessByte), flags(flags) {}
 	};
 
 	struct __attribute__((packed)) GdtTssEntry {
@@ -52,7 +70,7 @@ namespace hal::x86_64 {
 
 		constexpr GdtTssEntry() = default;
 
-		GdtTssEntry(Tss const& tss)
+		explicit GdtTssEntry(Tss const& tss)
 			: len(sizeof(Tss)),
 			  baseLow16((size_t)&tss & 0xffff),
 			  baseMid8(((size_t)&tss >> 16) & 0xff),
@@ -66,54 +84,44 @@ namespace hal::x86_64 {
 	struct __attribute__((packed)) Gdt {
 		static constexpr size_t LEN = 6;
 
-		enum Selector {
-			ZERO = 0,
-			KCODE = 1,
-			KDATA = 2,
-			UDATA = 3,
-			UCODE = 4,
-			TSS = 5,
-		};
+		GdtEntry entries[6];
 
-		enum Flags : u32 {
-			SEGMENT = 0b00010000,
-			PRESENT = 0b10000000,
-			USER = 0b01100000,
-			EXECUTABLE = 0b00001000,
-			READ_WRITE = 0b00000010,
-		};
-
-		enum Granularity {
-			LONG_MODE = 0b10,
-		};
-
-		Karm::Array<GdtEntry, Gdt::LEN - 1> entries = {
+		/*Karm::Array<GdtEntry, Gdt::LEN - 1> entries = {
 			GdtEntry{},
-			{Gdt::Flags::PRESENT | Gdt::Flags::SEGMENT | Gdt::Flags::READ_WRITE | Gdt::Flags::EXECUTABLE, Granularity::LONG_MODE},
-			{Gdt::Flags::PRESENT | Gdt::Flags::SEGMENT | Gdt::Flags::READ_WRITE, 0},
-			{Gdt::Flags::PRESENT | Gdt::Flags::SEGMENT | Gdt::Flags::READ_WRITE | Gdt::Flags::USER, 0},
-			{Gdt::Flags::PRESENT | Gdt::Flags::SEGMENT | Gdt::Flags::READ_WRITE | Gdt::Flags::EXECUTABLE | Gdt::Flags::USER, Granularity::LONG_MODE},
-		};
+			{AccessBytes::PRESENT | AccessBytes::CD_SEGMENT | AccessBytes::READ_WRITE | AccessBytes::EXECUTABLE, Flags::LONG_MODE},
+			{AccessBytes::PRESENT | AccessBytes::CD_SEGMENT | AccessBytes::READ_WRITE, 0},
+			{AccessBytes::PRESENT | AccessBytes::CD_SEGMENT | AccessBytes::READ_WRITE | AccessBytes::USER, 0},
+			{AccessBytes::PRESENT | AccessBytes::CD_SEGMENT | AccessBytes::READ_WRITE | AccessBytes::EXECUTABLE | AccessBytes::USER, Flags::LONG_MODE},
+		};*/
 
 		GdtTssEntry tssEntry;
 
-		Gdt(Tss const& tss)
-			: tssEntry(tss) {}
+		explicit Gdt(Tss const& tss): tssEntry(tss) {}
 	};
 
 	struct __attribute__((packed)) GdtDesc {
 		u16 limit;
 		u64 base;
 
-		GdtDesc(Gdt const& base)
-			: limit(sizeof(Gdt) - 1),
-			  base(reinterpret_cast<size_t>(&base)) {}
-
-		void load() const { _gdtLoad(this); }
+		explicit GdtDesc(Gdt const& base): limit(sizeof(Gdt) - 1), base(reinterpret_cast<size_t>(&base)) {}
 	};
 
 	class GdtManager {
+		public:
+			void initGdt();
+			void initTss();
+			void loadGdt();
+			void updateTss();
+			Gdt getGdt();
+			Tss getTss();
 
+		private:
+			void addGdtEntry(u8 flags, u8 granularity);
+			void addTssEntry();
+
+			Gdt gdtInstance;
+			GdtDesc gdtDescriptor;
+			Tss tssInstance;
 	};
 }
 
