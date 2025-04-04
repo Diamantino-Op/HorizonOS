49626a161580ca9bf4a48e7905084e0642379b73
Diamantino-Op <diamantino.op@gmail.com>
2025-04-04 22:11:56 +0200

    Update

--- src/kernel/kernel-x86_64/src/memory/Paging.s
+++ src/kernel/kernel-x86_64/src/memory/Paging.s
@@ -1,5 +1,4 @@
 .global initPagingAsm
 initPagingAsm:
     // Replace 'pml4_table' with the appropriate physical address (and flags, if applicable)
-    mov rax, [rdi]
-    mov cr3, rax
+    mov cr3, [rdi]
