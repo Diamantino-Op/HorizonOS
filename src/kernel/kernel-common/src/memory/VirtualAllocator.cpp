293f8962a4a8738fc490f6bc7ec5fa2af04be60e
Diamantino <Diamantino.op@gmail.com>
2025-05-29 11:49:17 +0200

    Update

--- src/kernel/kernel-common/src/memory/VirtualAllocator.cpp
+++ src/kernel/kernel-common/src/memory/VirtualAllocator.cpp
@@ -108,6 +108,8 @@ namespace kernel::common::memory {
 		while (current != nullptr) {
 			if (current->free and current->size >= alignedSize) {
 				if (current->size > pageSize * 1000) {
+					fixBlock(ctx, current);
+
 					CommonMain::getTerminal()->error("AN 1 - Block 0x%.16lx is too big: %lu bytes", "VirtualAllocator", reinterpret_cast<u64>(current), current->size);
 				}
 
