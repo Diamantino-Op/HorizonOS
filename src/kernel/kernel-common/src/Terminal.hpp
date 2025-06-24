8e0cd50b229663f2f1914864fafb2cd8877017d1
Diamantino <diamantino.op@gmail.com>
2025-06-25 01:52:57 +0200

    Update

--- src/kernel/kernel-common/src/Terminal.hpp
+++ src/kernel/kernel-common/src/Terminal.hpp
@@ -10,43 +10,8 @@
 
 #include "threading/Scheduler.hpp"
 
-#define PRINTF_STATE_NORMAL       0
-#define PRINTF_STATE_LENGTH       1
-#define PRINTF_STATE_LENGTH_LONG  2
-#define PRINTF_STATE_LENGTH_SHORT 3
-#define PRINTF_STATE_SPEC         4
-
-#define PRINTF_LENGTH_DEFAULT     0
-#define PRINTF_LENGTH_SHORT_SHORT 1
-#define PRINTF_LENGTH_SHORT       2
-#define PRINTF_LENGTH_LONG        3
-#define PRINTF_LENGTH_LONG_LONG   4
-
 namespace kernel::common {
     using namespace threading;
-
-    enum TextFormatting {
-        Regular,
-        Bold,
-        LowIntensity,
-        Italic,
-        Underline,
-        Blinking,
-        Reverse,
-        Background,
-        Invisible
-    };
-
-    enum TextColor {
-        Black,
-        Red,
-        Green,
-        Yellow,
-        Blue,
-        Magenta,
-        Cyan,
-        White
-    };
 
     class Terminal {
     public:
@@ -73,12 +38,6 @@ namespace kernel::common {
         void errorNoLock(const char *format, const char *id, ...);
 
         ExecutionNode *getCurrentCore();
-
-        //char* getFormat(const char* mainFormat, ...);
-
-        //const char* getTextFormatting(TextFormatting format);
-        //const char* getTextColor(TextColor format);
-        //const char* getBackgroundColor(TextColor format);
 
     private:
         static flanterm_context *flantermCtx;
