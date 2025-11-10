bf904378be885143cf1b669aaac930a1f077d041
Diamantino-Op <diamantino.op@gmail.com>
2025-11-10 17:25:07 +0100

    Update

--- src/kernel/kernel-common/src/programs/Elf.cpp
+++ src/kernel/kernel-common/src/programs/Elf.cpp
@@ -1,1 +1,91 @@
 #include "Elf.hpp"
+
+#include "CommonMain.hpp"
+
+namespace kernel::common::programs {
+	bool Elf::isElf(const ElfCommonHeader *elfHeader) {
+		if(not elfHeader) {
+			return false;
+		}
+
+		if(elfHeader->elfIdentity[ElfIdent::ELF_ID_MAG0] != ElfMagic0) {
+			CommonMain::getTerminal()->error("ELF Header Magic 0 is incorrect!", "Elf Loader");
+
+			return false;
+		}
+
+		if(elfHeader->elfIdentity[ElfIdent::ELF_ID_MAG1] != ElfMagic1) {
+			CommonMain::getTerminal()->error("ELF Header Magic 1 is incorrect!", "Elf Loader");
+
+			return false;
+		}
+
+		if(elfHeader->elfIdentity[ElfIdent::ELF_ID_MAG2] != ElfMagic2) {
+			CommonMain::getTerminal()->error("ELF Header Magic 2 is incorrect!", "Elf Loader");
+
+			return false;
+		}
+
+		if(elfHeader->elfIdentity[ElfIdent::ELF_ID_MAG3] != ElfMagic3) {
+			CommonMain::getTerminal()->error("ELF Header Magic 3 is incorrect!", "Elf Loader");
+
+			return false;
+		}
+
+		return true;
+	}
+
+	bool Elf::isSupported(const ElfCommonHeader *elfHeader) {
+		if(not isElf(elfHeader)) {
+			CommonMain::getTerminal()->error("Invalid ELF file!", "Elf Loader");
+
+			return false;
+		}
+
+		if(elfHeader->elfIdentity[ElfIdent::ELF_ID_DATA] != ElfLSB) {
+			CommonMain::getTerminal()->error("Unsupported ELF File byte order!", "Elf Loader");
+
+			return false;
+		}
+
+		if(elfHeader->elfMachine != ElfX86Machine) {
+			CommonMain::getTerminal()->error("Unsupported ELF File target!", "Elf Loader");
+
+			return false;
+		}
+
+		if(elfHeader->elfIdentity[ElfIdent::ELF_ID_VERSION] != ElfCurrVersion) {
+			CommonMain::getTerminal()->error("Unsupported ELF File version!", "Elf Loader");
+
+			return false;
+		}
+
+		if(elfHeader->elfType != ElfType::ET_REL and elfHeader->elfType != ElfType::ET_EXEC) {
+			CommonMain::getTerminal()->error("Unsupported ELF File type!", "Elf Loader");
+
+			return false;
+		}
+
+		return true;
+	}
+
+	bool Elf::is64Bit(const ElfCommonHeader *elfHeader) {
+		if (elfHeader->elfIdentity[ElfIdent::ELF_ID_CLASS] != Elf64Bit) {
+			CommonMain::getTerminal()->warn("ELF is not 64 bit!", "Elf Loader");
+
+			return false;
+		}
+
+		return true;
+	}
+
+	bool Elf::is32Bit(const ElfCommonHeader *elfHeader) {
+		if (elfHeader->elfIdentity[ElfIdent::ELF_ID_CLASS] != Elf32Bit) {
+			CommonMain::getTerminal()->warn("ELF is not 32 bit!", "Elf Loader");
+
+			return false;
+		}
+
+		return true;
+	}
+}
--- src/kernel/kernel-common/src/programs/Elf.hpp
+++ src/kernel/kernel-common/src/programs/Elf.hpp
@@ -1,4 +1,109 @@
 #ifndef KERNEL_COMMON_ELF_HPP
 #define KERNEL_COMMON_ELF_HPP
 
+#include "Types.hpp"
+
+namespace kernel::common::programs {
+    constexpr u8 ElfIdentitySize = 16;
+
+    constexpr u8 ElfMagic0 = 0x07;
+    constexpr u8 ElfMagic1 = 'E';
+    constexpr u8 ElfMagic2 = 'L';
+    constexpr u8 ElfMagic3 = 'F';
+
+    constexpr u8 Elf64Bit = 2;
+    constexpr u8 Elf32Bit = 1;
+
+    constexpr u8 ElfLSB = 1;
+    constexpr u8 ElfMSB = 2;
+
+    constexpr u16 ElfX86Machine = 3;
+    constexpr u16 ElfArmMachine = 40;
+
+    constexpr u32 ElfCurrVersion = 1;
+
+    typedef u64 Elf64Addr;
+    typedef u32 Elf32Addr;
+
+    typedef u64 Elf64Off;
+    typedef u32 Elf32Off;
+
+    typedef i64 ElfSXWord;
+    typedef i32 ElfSWord;
+
+    typedef u64 ElfXWord;
+    typedef u32 ElfWord;
+
+    typedef u16 ElfHalf;
+    typedef u8 ElfByte;
+    typedef u16 ElfSection;
+
+    struct ElfCommonHeader {
+        u8		    elfIdentity[ElfIdentitySize];
+        ElfHalf	    elfType;
+        ElfHalf	    elfMachine;
+        ElfWord	    elfVersion;
+    };
+
+    struct Elf32Header {
+        ElfCommonHeader commonHeader;
+        Elf32Addr	    elfEntryAddr;
+        Elf32Off	    elfProgHeaderOff;
+        Elf32Off	    elfSectionHeaderOff;
+        ElfWord	        elfFlags;
+        ElfHalf	        elfHeaderSize;
+        ElfHalf         elfProgHeaderSize;
+        ElfHalf	        elfProgHeaderAmount;
+        ElfHalf	        elfSectionHeaderSize;
+        ElfHalf	        elfSectionHeaderAmount;
+        ElfHalf	        elfStringTableSectionHeaderIndex;
+    };
+
+    struct Elf64Header {
+        ElfCommonHeader commonHeader;
+        Elf64Addr	    elfEntryAddr;
+        Elf64Off	    elfProgHeaderOff;
+        Elf64Off	    elfSectionHeaderOff;
+        ElfWord	        elfFlags;
+        ElfHalf	        elfHeaderSize;
+        ElfHalf         elfProgHeaderSize;
+        ElfHalf	        elfProgHeaderAmount;
+        ElfHalf	        elfSectionHeaderSize;
+        ElfHalf	        elfSectionHeaderAmount;
+        ElfHalf	        elfStringTableSectionHeaderIndex;
+    };
+
+    enum ElfIdent {
+        ELF_ID_MAG0		    = 0, // 0x7F
+        ELF_ID_MAG1		    = 1, // 'E'
+        ELF_ID_MAG2		    = 2, // 'L'
+        ELF_ID_MAG3		    = 3, // 'F'
+        ELF_ID_CLASS	    = 4, // Architecture (32/64)
+        ELF_ID_DATA		    = 5, // Byte Order
+        ELF_ID_VERSION	    = 6, // ELF Version
+        ELF_ID_ABI	        = 7, // OS Specific
+        ELF_ID_ABI_VERSION	= 8, // OS Specific
+        ELF_ID_PADDING		= 9  // Padding
+    };
+
+    enum ElfType {
+        ET_NONE		= 0, // Unkown Type
+        ET_REL		= 1, // Relocatable File
+        ET_EXEC		= 2  // Executable File
+    };
+
+    class Elf {
+    public:
+        static bool isElf(const ElfCommonHeader *elfHeader);
+
+        static bool isSupported(const ElfCommonHeader *elfHeader);
+
+        static bool is64Bit(const ElfCommonHeader *elfHeader);
+
+        static bool is32Bit(const ElfCommonHeader *elfHeader);
+
+    private:
+    };
+}
+
 #endif
